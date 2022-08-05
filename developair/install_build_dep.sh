dd5158290d337516d1f72d135d5373263359f3ef
PinoPair <jbinchi@developair.es>
2022-08-05 14:26:54 +0200

    updated dep script

--- developair/install_build_dep.sh
+++ developair/install_build_dep.sh
@@ -15,35 +15,33 @@ fi
 echo 'deb [trusted=true] http://www.lrde.epita.fr/repo/debian/ unstable/' >> /etc/apt/sources.list && \
     apt-get update && \
     RUNLEVEL=1 DEBIAN_FRONTEND=noninteractive \
-    apt-get install -y \
-    wget git \
+    apt-get install -y --no-install-recommends \
+    wget git build-essential debhelper \
     emacs-nox emacs-goodies-el org-mode \
     make gcc g++ \
     swig4.0 \
-    autoconf automake libtool \
+    autoconf automake libtool libltdl-dev \
     flex bison \
-    graphviz imagemagick optipng \
+    graphviz imagemagick \
     doxygen \
     help2man pandoc groff \
     python-all-dev python3-all-dev python3-pandas python3-ipykernel \
     python3-nbconvert python3-entrypoints python3-pygments ipython3 \
-    r-base-core r-recommended- r-base-dev- r-base-html- \
+    python3-pip pypy3 pypy3-dev jupyter jupyter-nbconvert \
+    r-base-core r-cran-data.table r-cran-ggplot2 \
     latexmk texlive-latex-extra texlive-fonts-extra \
     fonts-roboto \
     texlive-science \
-    texlive-latex-extra-doc- texlive-fonts-extra-doc- \
-    texlive-latex-base-doc- texlive-latex-recommended-doc- \
-    texlive-pictures-doc- texlive-pstricks-doc- \
-    texlive-fonts-recommended-doc- \
-    devscripts git-buildpackage cmake \
+    devscripts git-buildpackage cmake dh-python \
     libbdd0c2 libbdd-dev zlib1g-dev \
     locales \
     divine-ltsmin spins spin \
-    default-jdk ant dot2tex pdf2svg lcov \
-    g++-mingw-w64-i686 gcc-snapshot \
-    dh-python
+    default-jdk ant dot2tex pdf2svg lcov npm \
+    g++-mingw-w64-i686 patch \
+    dh-python 
 
 apt-get clean
+rm -rf /var/lib/apt/lists/* && \
 sed -i 's/# \+\(en_US.UTF.*\)/\1/' /etc/locale.gen
 locale-gen
 ipython3 -c 'exit;'
@@ -51,27 +49,9 @@ ipython3 -c 'exit;'
 
 ### nice part ends here, time to compile
 
-#!/bin/sh
-
-set -e  # Abort on any error
-set -x  # Show each instruction as is it run
-
-# GNU Libtool
-V=2.4.6
-wget http://ftpmirror.gnu.org/libtool/libtool-$V.tar.gz
-tar xvf libtool-$V.tar.gz
-cd libtool-$V
-# Debian has its own libltdl that we want to use when
-# building the Debian packages.
-./configure --disable-ltdl-install
-make -j4
-make install
-cd ..
-rm -rf libtool-$V
-
 # LTL2BA
-V=1.2b1
-wget http://www.lsv.ens-cachan.fr/~gastin/ltl2ba/ltl2ba-$V.tar.gz
+V=1.3
+wget https://www.lrde.epita.fr/dload/spot/deps/ltl2ba-$V.tar.gz
 tar xvf ltl2ba-$V.tar.gz
 cd ltl2ba-$V
 make -j4
@@ -81,7 +61,7 @@ rm -rf ltl2ba-$V ltl2ba-$V.tar.gz
 
 # LTL3BA
 V=1.1.3
-wget http://sourceforge.net/projects/ltl3ba/files/ltl3ba/1.1/ltl3ba-$V.tar.gz
+wget https://www.lrde.epita.fr/dload/spot/deps/ltl3ba-$V.tar.gz
 tar xvf ltl3ba-$V.tar.gz
 cd ltl3ba-$V
 make -j4
@@ -90,8 +70,8 @@ cd ..
 rm -rf ltl3ba-$V ltl3ba-$V.tar.gz
 
 # LTL3DRA
-V=0.2.5
-wget http://sourceforge.net/projects/ltl3dra/files/ltl3dra-$V.tar.gz
+V=0.3.0
+wget https://github.com/xblahoud/ltl3dra/archive/v$V.tar.gz -O ltl3dra-$V.tar.gz
 tar xvf ltl3dra-$V.tar.gz
 cd ltl3dra-$V
 make -j4
@@ -100,8 +80,8 @@ cd ..
 rm -rf ltl3dra-$V ltl3dra-$V.tar.gz
 
 # ltl2dstar
-V=0.5.3
-wget http://www.ltl2dstar.de/down/ltl2dstar-$V.tar.gz
+V=0.5.4
+wget https://www.lrde.epita.fr/dload/spot/deps/ltl2dstar-$V.tar.gz
 tar xvf ltl2dstar-$V.tar.gz
 cd ltl2dstar-$V/src
 make -j4
@@ -110,31 +90,32 @@ cd ../..
 rm -rf ltl2dstar-$V ltl2dstar-$V.tar.gz
 
 # glugose 4.0
-wget http://www.labri.fr/perso/lsimon/downloads/softwares/glucose-syrup.tgz
+wget https://www.lrde.epita.fr/dload/spot/deps/glucose-syrup.tgz
 tar xvf glucose-syrup.tgz
 cd glucose-syrup/simp
 make -j4
 mv glucose /usr/local/bin
 cd ../..
-rm -rf glucose-syrup
+rm -rf glucose-syrup glucose-syrup.tgz
 
 # modella 1.5.9
-wget http://web.archive.org/web/20080607170403/http://www.science.unitn.it/~stonetta/modella/MoDeLLa-1.5.9.tar.gz
+wget https://www.lrde.epita.fr/dload/spot/deps/MoDeLLa-1.5.9.tar.gz
 tar xvf MoDeLLa-1.5.9.tar.gz
 cd MoDeLLa-1.5.9
+perl -pi -e 's/CFLAGS = .*$/$& -fcommon/' Makefile
 make -j4
 mv modella /usr/local/bin
 cd ..
-rm -rf MoDeLLa-1.5.9
+rm -rf MoDeLLa-1.5.9 MoDeLLa-1.5.9.tar.gz
 
 # lbt 1.2.2
-wget http://www.tcs.hut.fi/Software/maria/src/lbt-1.2.2.tar.gz
+wget https://www.lrde.epita.fr/dload/spot/deps/lbt-1.2.2.tar.gz
 tar xvf lbt-1.2.2.tar.gz
 cd lbt-1.2.2
 make -j4
 cp lbt /usr/local/bin
 cd ..
-rm -rf lbt-1.2.2
+rm -rf lbt-1.2.2 lbt-1.2.2.tar.gz
 
 # lbtt 1.2.1a
 wget http://www.lrde.epita.fr/dload/spot/lbtt-1.2.1a.tar.gz
@@ -144,25 +125,26 @@ cd lbtt-1.2.1a
 make -j4
 make install
 cd ..
-rm -rf lbtt-1.2.1a
+rm -rf lbtt-1.2.1a lbtt-1.2.1a.tar.gz
 
-# Spin 6.4.6
-# wget http://spinroot.com/spin/Src/src646.tar.gz
-# tar xvf src646.tar.gz
-# cd Src6.4.6
-# make -j4
-# mv spin /usr/local/bin
-# cd ..
-# rm -rf Src6.4.6
+# valgrind 3.17.0
+# Needed to read DWARF5 from gcc 11
+wget https://sourceware.org/pub/valgrind/valgrind-3.17.0.tar.bz2
+tar xvf valgrind-3.17.0.tar.bz2
+cd valgrind-3.17.0
+./configure
+make -j4
+make install
+cd ..
+rm -rf valgrind-3.17.0 valgrind-3.17.0.tar.bz2
 
 # GAL2C
-wget http://teamcity.lrde.epita.fr/repository/download/Spot_Gal2cStretch/.lastSuccessful/gal2c?guest=1 -O gal2c
+wget -O gal2c 'https://gitlab.lrde.epita.fr/spot/spot-gal2c/-/jobs/artifacts/master/raw/libITS/bin/gal2c?job=gal2c'
 chmod a+x gal2c
 mv gal2c /usr/local/bin
 
-# dot2tex from git, because the last release from 2014 has bugs
-git clone https://github.com/kjellmf/dot2tex.git
-cd dot2tex
-python setup.py install
-cd ..
-rm -rf dot2tex
+# dot2tex
+pip3 install dot2tex
+
+# svgo
+npm install -g svgo@1.3.2
